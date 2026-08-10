12e2d85349edce23d2ece0c809be689d04bf7bf4
Ahmadx_x10 <ahmadabunassar194@gmail.com>
2026-08-10 20:22:57 +0300

    Update bot token retrieval method

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ async def give_money(ctx, member: discord.Member, amount: int):
         await ctx.send('ما عندك صلاحية يا غالي! ❌')
 
 # ⚠️ السطر هاد هو الأهم وهو اللي كان ناقص وبشغل البوت 24 ساعة
-# حط التوكن تبع البوت تبعك مكان كلمة YOUR_BOT_TOKEN_HERE
+# حط التوكن تبع البوت تبعك مكان كلمة 
 bot.run(os.environ.get('70a8582c0aada46ef125adebca1fc69d4716e5bbb9e5364e63e3e9fcbc21f8e0'))
 
 
